b7ff122202c63ff91b7d35f746f9bd0b49a1478f
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-27 11:15:03 +0200

    add firstnane in user class

--- app/models.py
+++ app/models.py
@@ -7,14 +7,13 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-     #...
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
     # role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    firstname = db.column(db.string(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
