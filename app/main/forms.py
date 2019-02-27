aebd1ae467c40f32b4d08dba21618da9ec9883bc
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-27 12:10:20 +0200

    add to import Emai in our forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email
 
 class ReviewForm(FlaskForm):
 
--- app/models.py
+++ app/models.py
@@ -44,3 +44,48 @@ posted = db.Column(db.DateTime,default=datetime.utcnow)
 user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 likes = db.Column(db.Integer)
 dislikes = db.Column(db.Integer)
+comments = db.relationship('Comment',backref =  'pitch_id',lazy = "dynamic")
+def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(category=category).all()
+        return pitches
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitch = Pitch.query.filter_by(id=id).first()
+
+        return pitch
+
+    @classmethod
+    def count_pitches(cls,uname):
+        user = User.query.filter_by(username=uname).first()
+        pitches = Pitch.query.filter_by(user_id=user.id).all()
+
+        pitches_count = 0
+        for pitch in pitches:
+            pitches_count += 1
+
+        return pitches_count
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch):
+        comments = Comment.query.filter_by(pitch_id=pitch).all()
+        return comments
