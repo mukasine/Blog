141503f3c430ad01a4565ed5b193d7749e689031
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-27 11:04:03 +0200

    add firstnane in user class

---
+++ app/main/views.py
@@ -0,0 +1,112 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+# from ..request import get_movies,get_movie,search_movie
+from .forms import ReviewForm,UpdateProfile
+from .. import db,photos
+from ..models import User
+from flask_login import login_required
+
+# Views
+# @app.route('/')
+# def index():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+
+#     message = 'Hello World'
+#     return render_template('index.html',message = message)
+@main.route('/')
+def index():
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title,)
+@main.route('/movies/<int:id>')
+def movies(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
+@main.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+@main.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update/pic',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+        form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+    
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form) 
+    
+
+@main.route('/user/<uname>/update/pic',methods = ['POST'])
+@login_required    
+def update_pic(uname): 
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+    
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
+export SECRET_KEY='0788504294ma0788896659pa'
+
+export MAIL_USERNAME='mclaremukasine@gmail.com'
+export MAIL_PASSWORD='0782586013'
+python3.6 manage.py server
