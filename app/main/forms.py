4a0f7af24512b9074b1ada295898bd2295bd5079
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-27 12:13:52 +0200

    add class loginform in our forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,6 +7,13 @@ class ReviewForm(FlaskForm):
     title = StringField('Review title',validators=[Required()])
     review = TextAreaField('Movie review', validators=[Required()])
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+ 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
